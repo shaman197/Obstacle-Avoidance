f5694d1160a98caf1ee62250587fb743ca2f5370
Boy Wu <boy_261@msn.com>
2017-08-09 17:18:08 +0200

    Base of assignment: - make environment - colliding objects
    Total work 30 min

---
+++ Assets/ObstacleAvoidanceBot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAvoidanceBot : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public float radius;
+    public float speed;
+    public Vector3 destination;
+
+    private SphereCollider spherecollider;
+    private bool canMove;
+
+	private void Start()
+    {
+        startPosition = transform.position;
+        spherecollider = GetComponent<SphereCollider>();
+        radius = spherecollider.radius;
+
+    }
+	
+	// Update is called once per frame
+	private void FixedUpdate()
+    {
+        if(canMove)
+        {
+            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * speed);
+        }
+	}
+
+    public void SetCanMove(bool value)
+    {
+        canMove = value;
+    }
+}
---
+++ Assets/ObstacleAvoidanceBotManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAvoidanceBotManager : MonoBehaviour
+{
+    private ObstacleAvoidanceBot[] bots;
+
+    private void Start()
+    {
+        bots = FindObjectsOfType<ObstacleAvoidanceBot>();
+
+    }
+
+    public void SetCanMove(bool value)
+    {
+        foreach(ObstacleAvoidanceBot bot in bots)
+        {
+            bot.SetCanMove(value);
+        }
+    }
+}
