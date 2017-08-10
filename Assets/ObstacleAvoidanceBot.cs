745ae6dc2269b1fbfb87f92758f05cade21daae5
Boy Wu <boy_261@msn.com>
2017-08-10 13:19:44 +0200

    First try dodging spheres 20 min

--- Assets/ObstacleAvoidanceBot.cs
+++ Assets/ObstacleAvoidanceBot.cs
@@ -8,16 +8,17 @@ public class ObstacleAvoidanceBot : MonoBehaviour
     public float radius;
     public float speed;
     public Vector3 destination;
+    public Vector3 nextPosition;
 
     private SphereCollider spherecollider;
     private bool canMove;
+    public GameObject otherSphere;
 
 	private void Start()
     {
         startPosition = transform.position;
         spherecollider = GetComponent<SphereCollider>();
         radius = spherecollider.radius;
-
     }
 	
 	// Update is called once per frame
@@ -25,12 +26,48 @@ public class ObstacleAvoidanceBot : MonoBehaviour
     {
         if(canMove)
         {
-            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * speed);
+            CheckObatacle();
+
+            if(otherSphere != null)
+            {
+                transform.RotateAround(otherSphere.transform.position, Vector3.forward, Time.deltaTime * speed);
+            }
+
+            //else
+            //{
+            //    transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * speed);
+            //    transform.LookAt(destination);
+            //}
         }
 	}
 
     public void SetCanMove(bool value)
     {
         canMove = value;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(spherecollider.center + transform.TransformPoint(nextPosition), radius);
+    }
+
+    public void CheckObatacle()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.TransformPoint(nextPosition) + spherecollider.center, radius);
+
+        //Debug.Log(transform.name + " " + hits.Length);
+
+        if (hits.Length >= 1)
+        {
+            foreach (Collider hit in hits)
+            {
+                if (hit.name != "Plane")
+                {
+                    otherSphere = hit.gameObject;
+                }
+            }
+        }
+
     }
 }
